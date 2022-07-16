065b59480ffd9a6a36d90183e1b3c9479935e5be
vagner <vagnerseibert@gmail.com>
2022-07-16 09:42:44 -0300

    Added characters. Added dice functions.

--- Assets/Scripts/Game/CharacterAttributes.cs
+++ Assets/Scripts/Game/CharacterAttributes.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "GMTK/Character")]
 public class CharacterAttributes : ScriptableObject {
-    
+    public string className;
+    public int hp = 10;
+    public int atk = 2;
+    public int move = 2;
 }
--- Assets/Scripts/Utils/DiceUtils.cs
+++ Assets/Scripts/Utils/DiceUtils.cs
@@ -6,4 +6,16 @@ public static class DiceUtils {
     public static int d4 => Random.Range(1, 5);
     public static int d6 => Random.Range(1, 7);
     public static int d8 => Random.Range(1, 9);
+
+    public static int[] d6x(this int k){
+        var r = new int[k];
+        for(int i=0; i<k; i++) r[i] = d6;
+        return r;
+    }
+
+    public static int sum(this int[] r){
+        var s = 0;
+        for(int i=0; i<r.Length; i++) s += r[i];
+        return s;
+    }
 }
